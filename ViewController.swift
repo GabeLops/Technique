ff8214d12adc74e305f78ca28a90374f60542e5f
GabeLops <gabrieldlops@gmail.com>
2020-01-27 23:53:29 -0800

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project15
+//
+//  Created by Gabriel Lops on 1/27/20.
+//  Copyright © 2020 Gabriel Lops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
