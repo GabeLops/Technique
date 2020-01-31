f5fb19e15103ae9da0732d54e441f50e8a476630
GabeLops <gabrieldlops@gmail.com>
2020-01-30 23:39:49 -0800

    add detailed view controller class

---
+++ detailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  detailViewController.swift
+//  project15
+//
+//  Created by Gabriel Lops on 1/30/20.
+//  Copyright © 2020 Gabriel Lops. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: tableview {
+
+}
